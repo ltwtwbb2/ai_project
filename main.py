680b40e112df6cce28e156d3b7dc30eb742f7166
ltwtwbb2 <ltwtwbbbb@gmail.com>
2026-05-13 13:09:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ a=st.text_input('이름을 입력하세요')
 b=st.selectbox('좋아하는 음식을 선택하세요',['치킨','스테이크','햄버거'])
 if st.button('인사말 생성'):
   st.write(a+'님! 안녕하세요')
+st.info('반갑습니다')
+st.warning(b+'좋아하시는군요!')
+st.error('잘 지내봐요!!')
